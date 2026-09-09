aef746dbd24821ab5ad386dbacc7382afa9a55a8
Dzmitryi Kharlanau <dkharlanau@gmail.com>
2026-09-09 16:19:52 +0300

    Expose Technical Integrity from arwp CLI

--- bin/arwp.mjs
+++ bin/arwp.mjs
@@ -2,6 +2,8 @@
 
 if (process.argv[2] === 'site-gate') {
   await import('./arwp-site-gate.mjs');
+} else if (process.argv[2] === 'technical-integrity') {
+  await import('./arwp-technical-integrity.mjs');
 } else {
   await import('./arwp-core.mjs');
 }
